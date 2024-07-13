b9eb7bd92a59086f68a1f2c2f77dc877866804c5
unknown <arteaga980910@gmail.com>
2024-07-13 14:46:18 -0500

    Herramienta para juntar contenidos de archivos planos

---
+++ ForJoinScripts/ForJoinScripts/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ForJoinScripts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string originPath = @"C:\Users\Udenar\Documents\AFACs_tools\source";
+            DirectoryInfo originDirectory = new DirectoryInfo(originPath);
+
+            string towardPath = @"C:\Users\Udenar\Documents\AFACs_tools\combined\join_scripts.sql";
+            FileInfo towardFile = new FileInfo(towardPath);
+            if (!towardFile.Exists)
+            {
+                towardFile.Create();
+            }
+
+            var directories = originDirectory.GetDirectories();
+            var directorios = Directory.GetDirectories(originPath);
+            int contador = 0;
+            using (StreamWriter writer = new StreamWriter(towardPath, append: true))
+            { 
+                foreach (var item in directories.OrderBy(x => x.Name))
+                {
+                    DirectoryInfo insideDirectory = new DirectoryInfo(item.FullName);
+                    var insideDirectories = insideDirectory.GetFiles();
+                    foreach(var iItem in insideDirectories)
+                    {
+                        FileInfo insideFile = new FileInfo(iItem.FullName);
+                        Console.WriteLine(item.FullName);
+                        using (StreamReader reader = insideFile.OpenText())
+                        {
+                            string fileContent = reader.ReadToEnd();
+                            writer.WriteLine(fileContent);
+                            writer.WriteLine();
+                        }
+                    }
+                }            
+            }
+
+            using (StreamReader reader = towardFile.OpenText())
+            {
+                string fileContent = reader.ReadToEnd();
+                Console.WriteLine(fileContent);
+            }
+            Console.WriteLine($"Continuar ({contador}):");
+            var respuesta = Console.ReadLine();
+
+
+        }
+    }
+}
